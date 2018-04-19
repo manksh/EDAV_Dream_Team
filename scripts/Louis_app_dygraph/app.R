9125b1289dcc51dc2eb9760192444e40f270ee03
Louis Massera <louismassera@gmail.com>
2018-04-18 22:40:52 -0400

    shiny dygraph added

---
+++ scripts/Louis_app_dygraph/app.R
@@ -0,0 +1,113 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+library(memisc) 
+source('global.R')
+
+######### Load Data ############
+
+word_count <- read.csv("../../data/Louis/word_count_per_year.csv")
+
+##############UI ########################
+ui <- fluidPage(
+  # Application title
+  titlePanel("Word count over time"),
+  
+  sidebarLayout(
+    # Sidebar with a slider and selection inputs
+    sidebarPanel(
+      selectInput("word1", 
+                  "Choose a word:",
+                  choices = c("boy" = "boy",
+                              "girl" = "girl",
+                              "like" = "like",
+                              "love" = "love",
+                              "man" = "man",
+                              "peace" = "peace",
+                              "war" = "war",
+                              "woman" = "woman",
+                              " " = " "), c("love")),
+      selectInput("word2", "Choose another word:", choices = c("boy" = "boy",
+                                                               "girl" = "girl",
+                                                               "like" = "like",
+                                                               "love" = "love",
+                                                               "man" = "man",
+                                                               "peace" = "peace",
+                                                               "war" = "war",
+                                                               "woman" = "woman",
+                                                               " " = " "), c("like"))
+    ),
+    
+    mainPanel(
+      plotOutput("word_graph"),
+      uiOutput("video", width="100px", height="100px")
+    )
+  )
+)
+
+
+
+
+
+server <- function(input, output) {
+  
+  
+  ####################
+  #Dashboard 3 graphs
+
+  #Line Graphs
+  
+  output$word_graph <- renderPlot({
+      
+      word_1 <- input$word1
+      word_2 <- input$word2
+      if (word_1 == " " & word_2 == " "){
+        output$video <- renderUI({
+          HTML(paste0('<iframe width="500" height="300" src="https://www.youtube.com/embed/SHJ1HL4eMLQ", frameborder="0" allowfullscreen></iframe>'))
+          }
+        )
+        output$selected_var <- renderText({ 
+          
+        })
+      }
+      else if (word_1 == " "){
+        data = filter(word_count, word_count$word == word_2)
+        ggplot(data, aes(x = year, y = count)) +
+          geom_line() +
+          theme(legend.position="bottom") +
+          ggtitle(word_2) +
+          theme(plot.title = element_text(hjust = 0.5)) +
+          xlab("Year") + 
+          ylab("Count")
+        output$video <- renderUI({})
+        
+      }
+      else if (word_2 == " " | word_1 == word_2){
+        data = filter(word_count, word_count$word == word_1)
+        ggplot(data, aes(x = year, y = count)) +
+          geom_line() +
+          theme(legend.position="bottom") +
+          ggtitle(word_1) +
+          theme(plot.title = element_text(hjust = 0.5)) +
+          xlab("Year") + 
+          ylab("Count")
+        output$video <- renderUI({})
+        
+      }
+      else {
+        data = filter(word_count, word_count$word == word_1 | word_count$word == word_2)
+        ggplot(data, aes(x = year, y = count, col = word)) +
+          geom_line() +
+          theme(legend.position="bottom") +
+          ggtitle(paste(word_1,word_2, sep="/")) +
+          theme(plot.title = element_text(hjust = 0.5)) +
+          xlab("Year") + 
+          ylab("Count")
+        output$video <- renderUI({})
+        
+      }
+  })
+  #End marker for server         
+}
+
+shinyApp(ui, server)
